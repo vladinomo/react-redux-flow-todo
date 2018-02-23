d561cb99e58bcd03b4cb9f4b30ba4b8245703a23
vladinomo <reasonistreason.t.rex@gmail.com>
2018-02-23 12:35:35 +0900

    delete codes related to service worker

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
     "extends": "airbnb",
+    "env": {
+        "browser": true,
+        "jasmine": true
+    },
     "rules": {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     },
