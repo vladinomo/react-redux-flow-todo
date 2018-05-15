8d43f20d0c2889a037a208f60de62f06e126514e
vladinomo <reasonistreason.t.rex@gmail.com>
2018-05-16 08:14:27 +0900

    wip: add type anotation to files related to counter
    todo: 1.fix code formatter for flow 2.add annotations to index reducer and components

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,10 +1,31 @@
+// @flow
+
 // actions
 const ADD = 'counter/add';
 const INCREMENT = 'counter/increment';
 const DECREMENT = 'counter/decrement';
 
+// types
+export type State = {
+  value: number,
+};
+
+export type Action =
+  | {
+      type: typeof ADD,
+      payload: {
+        amount: number,
+      },
+    }
+  | {
+      type: typeof INCREMENT,
+    }
+  | {
+      type: typeof DECREMENT,
+    };
+
 // Action Creator
-export function add(amount) {
+export function add(amount: number) {
   return {
     type: ADD,
     payload: {
@@ -26,11 +47,11 @@ export function decrease() {
 }
 
 // reducer
-const initialState = {
+const initialState: State = {
   value: 0,
 };
 
-export default (state = initialState, action) => {
+export default (state: State = initialState, action: Action) => {
   switch (action.type) {
     case ADD:
       return {
